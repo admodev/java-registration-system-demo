bf5902d269fac9550a221b5c9619f27c5fd65970
admodev <admodevcodes@outlook.com>
2022-06-21 15:14:46 -0300

    add registration controller, request and service

---
+++ src/main/java/com/admodev/registrationsystem/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.admodev.registrationsystem.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "Works!";
+    }
+}
